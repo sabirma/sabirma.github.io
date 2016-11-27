c42897430c978af3f2def0f43f783208bf58d48b
jithinbp <jithinbp@gmail.com>
2016-11-27 13:59:28 +0530

    email test. map set

--- js/main.js
+++ js/main.js
@@ -198,14 +198,15 @@ jQuery(function($) {'use strict';
 		event.preventDefault();
 		var form_status = $('<div class="form_status"></div>');
 		$.ajax({
-			url: $(this).attr('action'),
+			url: "sendemail.php",
 			beforeSend: function(){
 				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn() );
 			}
 		}).done(function(data){
-			form_status.html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+			form_status.html('<p class="text-success">Thank you for contacting us. We will get in touch with you soon</p>').delay(3000).fadeOut();
 		});
 	});
+
 
 	//Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
